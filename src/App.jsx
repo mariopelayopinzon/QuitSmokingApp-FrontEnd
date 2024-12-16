4dfca7aaa100ad1d81dfe74138c9c8ceccde95a3
mariopelayopinzon <mariopelayo.psic@gmail.com>
2024-12-16 17:38:36 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from './components/common/Navbar.js'
+
+const App = () => {
+  return (
+  <Navbar/>
+  )
+}
+
+export default App
---
+++ src/components/common/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from 'react-router-dom';
+import { FaHome, FaChartLine, FaUser } from 'react-icons/fa';
+import '../../assets/styles/components/_navbar.scss'
+
+const Navbar = () => {
+
+
+
+  return (
+    <nav className='navbar'>.
+    <ul>
+      <li>Home</li>
+      <li>Progress</li>
+      <li>Contact</li>
+      <li>About Us</li>
+    </ul>
+    </nav>
+  )
+}
+export default Navbar
